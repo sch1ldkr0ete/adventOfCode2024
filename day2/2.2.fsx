80cd88d43e7069e247c4153bc34cbdbb6fdcd492
Daniel Petro <daniel.petro@icloud.com>
2024-12-05 22:51:14 +0100

    feat: add solution for day 2 part 2

--- day2/2.2.fsx
+++ day2/2.2.fsx
@@ -3,22 +3,27 @@ let getSteps levels =
   |> Seq.pairwise
   |> Seq.map (fun (fst, snd) -> snd - fst)
 
-let isSafeChange down step =
+let isSafeChange step =
   if step = 0 then false
   elif abs step >= 1 && abs step <= 3 then true
-  elif down && step < 0 then true
-  elif not down && step > 0 then true
   else false
 
 let areSafeSteps steps =
-  let isSafeChangeTest = isSafeChange (Seq.head steps < 0)
-  not (steps |> Seq.exists (fun step -> not (isSafeChangeTest step)))
+  not (steps |> Seq.exists (fun step -> not (isSafeChange step)))
   && not ((steps |> Seq.exists (fun step -> step > 0)) && (steps |> Seq.exists (fun step -> step < 0)))
 
 
 System.IO.File.ReadLines("./2.1.input")
 |> Seq.map (fun line -> line.Split " " |> Array.map int)
-|> Seq.map getSteps
-|> Seq.filter areSafeSteps
+|> Seq.map (
+  fun levels ->
+  [0..(Array.length levels)-1]
+  |> Seq.map (
+    fun i ->
+    (Seq.removeAt i levels)
+    |> getSteps
+  )
+  |> Seq.exists (fun steps -> areSafeSteps steps))
+|> Seq.filter (fun isSafe -> isSafe)
 |> Seq.length
 |> printfn "%A"
