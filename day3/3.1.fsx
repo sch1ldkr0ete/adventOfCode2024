f0528f969aa9751f779ad46c9e7865cafc6d3ffe
Daniel Petro <daniel.petro@icloud.com>
2024-12-07 15:41:24 +0100

    feat: solve day 3 part 1

--- day3/3.1.fsx
+++ day3/3.1.fsx
@@ -1,17 +1,32 @@
 let text =
-  System.IO.File.ReadLines("./3.1.test")
+  System.IO.File.ReadLines("./3.1.input")
   |> Seq.reduce (fun acc line -> acc + line)
 
+let checkString testString =
+  let allowedDigits = [0..999] |> List.map (fun x -> string(x)) |> Set.ofList
+  if
+    String.filter (fun x -> x = ',') testString
+    |> String.length > 1 then false
+  else
+    let values = testString.Split(",")
+    Set.contains values[0] allowedDigits && Set.contains values[1] allowedDigits
+
+
 let rec getMulStartIndexes (text : string) indexes from =
-  let searchString = "mul("
-  let index = from + text.Substring(from).IndexOf(searchString)
-  printfn "%A" index
-  printfn "----"
+  let searchStringStart = "mul("
+  let searchStringEnd = ")"
+  let substringIndex = text.Substring(from).IndexOf(searchStringStart)
+  let index = from + substringIndex
   let nextFrom =
-    if index + searchString.Length < text.Length then index + searchString.Length
-    else searchString.Length - 1
-  if index = -1 then indexes
-  else getMulStartIndexes text (index::indexes) nextFrom
+    if index + searchStringStart.Length < text.Length then index + searchStringStart.Length
+    else searchStringStart.Length - 1
+  let indexEnd = nextFrom + text.Substring(nextFrom).IndexOf(searchStringEnd)
+  if  substringIndex = -1 || nextFrom = 0 || indexEnd = -1 then indexes
+  else getMulStartIndexes text (indexes @ [text.Substring(index + searchStringStart.Length, indexEnd - (index + searchStringStart.Length))]) nextFrom
 
 getMulStartIndexes text [] 0
-|> Seq.iter (fun index -> printfn "%A" index)
+|> Seq.filter checkString
+|> Seq.map (fun x -> x.Split(','))
+|> Seq.map (fun x -> int x[0] * int x[1])
+|> Seq.sum
+|> printfn "%A"
